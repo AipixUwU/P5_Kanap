cd19dea48ba2bfd73c27494329bac0ccbe0615fe
AipixUwU <113269618+AipixUwU@users.noreply.github.com>
2023-02-20 09:39:51 +0100

    update product page

--- front/js/script.js
+++ front/js/script.js
@@ -1,23 +1,24 @@
 const url = "http://localhost:3000/api/products";
 const card = document.getElementById("items");
 
-const getProducts = () => {
-    fetch(url)
-    .then(function(test) {
-        return test.json()
-    })
-    .then(function (data){
-       for(product in data) {
-        card.innerHTML += `<a href="./product.html?id=42">
-        <article>
-          <img src="${data[product].imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
-          <h3 class="productName"> ${data[product].name}</h3>
-          <p class="productDescription">${data[product].description}</p>
-        </article>
-      </a>`
-       }
-    })
+const getProducts = async () => {
+  return await fetch(url).then(response => response.json())
 
 }
 
-getProducts()
+const showProduct = async () => {
+  const products = await getProducts()
+
+  products.map(function(product) {
+    card.innerHTML += `<a href="./product.html?id=42">
+      <article>
+        <img src="${product.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+        <h3 class="productName"> ${product.name}</h3>
+        <p class="productDescription">${product.description}</p>
+      </article>
+    </a>`
+  })
+
+}
+
+showProduct()
