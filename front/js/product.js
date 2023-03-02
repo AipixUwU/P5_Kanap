baaa44bfd09cb20dbb70602498ae617132eec666
AipixUwU <113269618+AipixUwU@users.noreply.github.com>
2023-03-02 03:35:11 +0100

    fetch method change - addToCart

--- front/js/product.js
+++ front/js/product.js
@@ -7,22 +7,59 @@ const productTitle = document.getElementById("title");
 const productPrice = document.getElementById("price");
 const productDescription = document.getElementById("description");
 const productColor = document.getElementById("colors");
+const productQuantity = document.getElementById("quantity");
 
-const getProduct = async () => {
-    return await fetch(productURL).then(response => response.json())
-}
+// const addToCart = document.getElementById("addToCart")
 
-const productFetch = async () => {
-    const product = await getProduct()
+fetch(productURL)
+    .then(response => response.json())
+    .then(product => {
+
+        productPrice.innerHTML = `${product.price}`;
+        productTitle.innerHTML = `${product.name}`;
+        productDescription.innerHTML = `${product.description}`;
+        productImage.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
     
-    productPrice.innerHTML = `${product.price}`;
-    productTitle.innerHTML = `${product.name}`;
-    productDescription.innerHTML = `${product.description}`;
-    productImage.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+        product.colors.map(function(color) {
+            productColor.innerHTML += `<option value="${color}">${color}</option>`;
+        })
+    })
 
-    product.colors.map(function(color) {
-        productColor.innerHTML += `<option value="${color}">${color}</option>`;
-    })   
-}
 
-productFetch()
+    // Ajout au panier
+let addToCart = document.getElementById("addToCart")
+    addToCart.addEventListener("click", function(){
+
+        let choiceColor = productColor.value;
+        let choiceQuantity = productQuantity.value;
+        let getProductName = document.getElementById("title").textContent;
+        let getProductPrice = document.getElementById("price").textContent;
+
+
+            if (choiceColor == "") {
+                alert("Selectioner une couleur");
+            }
+
+                //Une couleur a été selectionner
+            else { 
+
+                if (choiceQuantity <= 0 || choiceQuantity > 100) {
+                    alert("Quantité incorrecte")
+                }   
+                // La quantité est compris entre 1 et 100
+                else { 
+                    console.log("Ajout au panier")
+
+                    let productSelect = {                        
+                        title: getProductName,
+                        color: choiceColor,
+                        price: getProductPrice,
+                        quantity: choiceQuantity,
+                    }
+
+                    // console.log(productSelect);
+                    let productSelectJSON = JSON.stringify(productSelect);
+                    localStorage.setItem(`product`, productSelectJSON);
+                }     
+            }
+})
--- front/js/script.js
+++ front/js/script.js
@@ -1,22 +1,17 @@
 const url = "http://localhost:3000/api/products";
 const card = document.getElementById("items");
 
-const getProducts = async () => {
-  return await fetch(url).then(response => response.json())
+  fetch(url)
+      .then(response => response.json())
+      .then(products => {
 
-}
-
-const showProduct = async () => {
-  const products = await getProducts()
-
-  products.map(function(product) {
-    card.innerHTML += `<a href="./product.html?id=${product._id}">
-      <article>
-        <img src="${product.imageUrl}" alt="${product.altTxt}">
-        <h3 class="productName"> ${product.name}</h3>
-        <p class="productDescription">${product.description}</p>
-      </article>
-    </a>`
-  })
-}
-showProduct()
+          products.map(function(product) {
+            card.innerHTML += `<a href="./product.html?id=${product._id}">
+              <article>
+                <img src="${product.imageUrl}" alt="${product.altTxt}">
+                <h3 class="productName"> ${product.name}</h3>
+                <p class="productDescription">${product.description}</p>
+              </article>
+            </a>`
+          })
+})
