13c8f1575c84a8fef43ece58a086ad5b80924298
AipixUwU <113269618+AipixUwU@users.noreply.github.com>
2023-03-11 09:46:22 +0100

    total quantity / price

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,24 +2,43 @@ const productsURL = `http://localhost:3000/api/products`;
 
 let showProduct = document.getElementById("cart__items");
 let getProducts = JSON.parse(localStorage.getItem(`product`));
-
+let selectProduct = [];
+let totalQuantity = document.getElementById("totalQuantity");
+let totalPrice = document.getElementById("totalPrice");
+let allPrice = 0;
+let allQuantity = 0;
 
 fetch(productsURL)
     .then(response => response.json())
-    .then(product => {
-      
-      getProducts.map(function(showCart) {
-        showProduct.innerHTML += 
+    .then(products => {
+
+      getProducts.map(function(showCart) {  
+        products.map(function(product) {
+
+          if (showCart.productID === product._id) {
+            selectProduct = product;
+          }  
+        })
+        
+          // PRIX TOTAL
+      allPrice += selectProduct.price * showCart.quantity;
+      totalPrice.innerHTML = allPrice;
+
+          // QUANTITEE TOTAL
+      allQuantity += showCart.quantity++;
+      totalQuantity.innerHTML = allQuantity;
+
+      showProduct.innerHTML +=
 
         `<article class="cart__item" data-id="${showCart.productID}" data-color="${showCart.color}">
         <div class="cart__item__img">
-          <img src="${product.imageUrl}" alt="${product.altTxt}">
+          <img src="${selectProduct.imageUrl}" alt="${selectProduct.altTxt}">
         </div>
         <div class="cart__item__content">
           <div class="cart__item__content__description">
-            <h2>Nom du produit</h2>
+            <h2>${selectProduct.name}</h2>
             <p>${showCart.color}</p>
-            <p>42,00 €</p>
+            <p>${selectProduct.price} €</p>
           </div>
           <div class="cart__item__content__settings">
             <div class="cart__item__content__settings__quantity">
@@ -32,6 +51,5 @@ fetch(productsURL)
           </div>
         </div>
       </article>`
-
+      })
     })
-  })
