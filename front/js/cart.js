0e0b92165cb237ebafbd698e8750541e8f0dc777
AipixUwU <113269618+AipixUwU@users.noreply.github.com>
2023-03-19 12:20:39 +0100

    fetch update (id only)

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,49 +4,36 @@ let getProducts = JSON.parse(localStorage.getItem(`product`));
 let selectProduct = [];
 let totalQuantity = document.getElementById("totalQuantity");
 let totalPrice = document.getElementById("totalPrice");
-totalQuantity.textContent = 0;
-totalPrice.textContent = 0;
 let allPrice = 0;
 let allQuantity = 0;
 let deleteItem = document.getElementsByClassName("deleteItem");
 
 
 
-// getProducts.map(function (canape) {
-//   let productID = canape.productID;
-//   let productURL = `http://localhost:3000/api/products/${productID}`;
-//   fetch(productURL)
-//     .then(response => response.json())
-//     .then(product => {
-//       console.log(product);
-//     })
-// })
-
-
-
-
-
-
-fetch(productsURL)
+getProducts.map(function (showCart) {
+  let productID = showCart.productID;
+  let productURL = `http://localhost:3000/api/products/${productID}`;
+  fetch(productURL)
     .then(response => response.json())
     .then(products => {
-        getProducts.map(function (showCart) {
-            products.map(function (product) {
+      console.log(products);
 
-                if (showCart.productID === product._id) {
-                    selectProduct = product;
-                }
-            })
-            // PRIX TOTAL
-            allPrice = Number(totalPrice.textContent) + selectProduct.price * showCart.quantity;
-            totalPrice.innerHTML = allPrice;
 
-            // QUANTITEE TOTAL
-            allQuantity = Number(totalQuantity.textContent) + showCart.quantity;
-            totalQuantity.innerHTML = allQuantity;
-            showProduct.innerHTML +=
 
-                `<article class="cart__item" data-id="${showCart.productID}" data-color="${showCart.color}">
+      if (showCart.productID === products._id) {
+        selectProduct = products;
+      }
+
+      // PRIX TOTAL
+      allPrice = Number(totalPrice.textContent) + selectProduct.price * showCart.quantity;
+      totalPrice.innerHTML = allPrice;
+
+      // QUANTITEE TOTAL
+      allQuantity = Number(totalQuantity.textContent) + Number(showCart.quantity);
+      totalQuantity.innerHTML = allQuantity;
+      showProduct.innerHTML +=
+
+        `<article class="cart__item" data-id="${showCart.productID}" data-color="${showCart.color}">
         <div class="cart__item__img">
           <img src="${selectProduct.imageUrl}" alt="${selectProduct.altTxt}">
         </div>
@@ -68,50 +55,47 @@ fetch(productsURL)
         </div>
       </article>`
 
-    })
+
+      Array.from(deleteItem).map(function (d) {
+        d.addEventListener("click", function () {
+
+          let articleParent = this.closest("article");
+          articleParent.remove("article");
+          console.log(articleParent.dataset.id);
+          console.log(articleParent.dataset.color);
 
 
-    Array.from(deleteItem).map(function (d) {
-
-        d.addEventListener("click", function(){
-
-            let articleParent = this.closest("article");
-            articleParent.remove("article");
-
-                console.log(articleParent.dataset.id);
-                console.log(articleParent.dataset.color);
         })
+      })
     })
 })
 
-  const submit = document.getElementById("order")
+const submit = document.getElementById("order")
 
-  submit.addEventListener("click", function() {
+submit.addEventListener("click", function () {
 
-      let contact = {
-        firstName: document.getElementById("firstName").value,
-        lastName: document.getElementById("lastName").value,
-        address: document.getElementById("address").value,
-        city: document.getElementById("city").value,
-        email: document.getElementById("email").value,
-    }
+  let contact = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    email: document.getElementById("email").value,
+  }
   // PRENOM / NOM / VILLE
   const name = /^[a-z ,.'-]+$/i;
 
   // EMAIL
   const email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-    if  (
-          (email.test(contact.email)) &
-          (name.test(contact.firstName)) &
-          (name.test(contact.lastName))
-        ) {
+  if (
+    (email.test(contact.email)) &
+    (name.test(contact.firstName)) &
+    (name.test(contact.lastName))
+  ) {
 
-        alert("Formulaire envoyé")
-      }
-      else {
+    alert("Formulaire envoyé")
+  }
+  else {
 
-        alert("Erreur");
-      }
+    alert("Erreur");
+  }
 
-    })
+})
