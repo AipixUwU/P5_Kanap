19e26b0567bd05ee5e2f397ff88123fb02e002b1
AipixUwU <113269618+AipixUwU@users.noreply.github.com>
2023-03-24 01:35:19 +0100

    Errors & Contact

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,45 +1,35 @@
-function affichagePanier (){
+function affichagePanier() {
+
+  const showProduct = document.getElementById("cart__items");
+  const totalQuantity = document.getElementById("totalQuantity");
+  const totalPrice = document.getElementById("totalPrice");
+  let getProducts = JSON.parse(localStorage.getItem(`product`));
+  let selectProduct = [];
+  let allPrice = 0;
+  let allQuantity = 0;
 
 
-let showProduct = document.getElementById("cart__items");
-let getProducts = JSON.parse(localStorage.getItem(`product`));
-let selectProduct = [];
-let totalQuantity = document.getElementById("totalQuantity");
-let totalPrice = document.getElementById("totalPrice");
-let allPrice = 0;
-let allQuantity = 0;
-let deleteItem = document.getElementsByClassName("deleteItem");
-
-showProduct.innerHTML = "";
-totalQuantity.textContent = allQuantity;
-totalPrice.textContent = allPrice;
+  //UPDATE
+  showProduct.innerHTML = "";
+  totalQuantity.textContent = allQuantity;
+  totalPrice.textContent = allPrice;
 
 
+  // BOUCLE QUI PERMET DE RECUPERER (DEPUIS API) ET AFFICHER LES PRODUITS QUI SONT DANS LE LOCALSTORAGE
+  getProducts.map(function (showCart) {
+    let productID = showCart.productID;
+    let productURL = `http://localhost:3000/api/products/${productID}`;
+    fetch(productURL)
+      .then(response => response.json())
+      .then(products => {
+        if (showCart.productID === products._id) {
+          selectProduct = products;
+        }
 
-getProducts.map(function (showCart) {
-  let productID = showCart.productID;
-  let productURL = `http://localhost:3000/api/products/${productID}`;
-  fetch(productURL)
-    .then(response => response.json())
-    .then(products => {
-      console.log(products);
 
+        showProduct.innerHTML +=
 
-
-      if (showCart.productID === products._id) {
-        selectProduct = products;
-      }
-
-      // PRIX TOTAL
-      allPrice = Number(totalPrice.textContent) + selectProduct.price * showCart.quantity;
-      totalPrice.innerHTML = allPrice;
-
-      // QUANTITEE TOTAL
-      allQuantity = Number(totalQuantity.textContent) + Number(showCart.quantity);
-      totalQuantity.innerHTML = allQuantity;
-      showProduct.innerHTML +=
-
-        `<article class="cart__item" data-id="${showCart.productID}" data-color="${showCart.color}">
+          `<article class="cart__item" data-id="${showCart.productID}" data-color="${showCart.color}">
         <div class="cart__item__img">
           <img src="${selectProduct.imageUrl}" alt="${selectProduct.altTxt}">
         </div>
@@ -61,57 +51,128 @@ getProducts.map(function (showCart) {
         </div>
       </article>`
 
+        // PRIX TOTAL
+        allPrice = Number(totalPrice.textContent) + selectProduct.price * showCart.quantity;
+        totalPrice.innerHTML = allPrice;
 
-      Array.from(deleteItem).map(function (d) {
-        d.addEventListener("click", function () {
-
-          let articleParent = this.closest("article");
-          articleParent.remove("article");
-          console.log(articleParent.dataset.id);
-          console.log(articleParent.dataset.color);
-
-          let dataColor = articleParent.dataset.color;
-          let dataId = articleParent.dataset.id;
-          let productIndex = getProducts.findIndex((element) => element.productID == dataId && element.color == dataColor);
-          getProducts.splice (productIndex, 1)
-          localStorage.setItem(`product`, JSON.stringify(getProducts));
-          affichagePanier();
-          console.log(getProducts);
+        // QUANTITEE TOTAL
+        allQuantity = Number(totalQuantity.textContent) + Number(showCart.quantity);
+        totalQuantity.innerHTML = allQuantity;
 
 
+        // BOUCLE PERMETTANT D'ACTUALISER LA QUANTITÉ DEPUIS LE PANIER
+        const itemQuantity = document.getElementsByClassName("itemQuantity");
+
+        Array.from(itemQuantity).map(function (q) {
+          q.addEventListener("change", function () {
+
+            let articleParent = this.closest("article");
+            let dataColor = articleParent.dataset.color;
+            let dataId = articleParent.dataset.id;
+
+            let productIndex = getProducts.findIndex((element) => element.productID == dataId && element.color == dataColor);
+            getProducts[productIndex].quantity = q.value;
+
+            localStorage.setItem(`product`, JSON.stringify(getProducts));
+            affichagePanier();
+
+          })
         })
+
+        // BOUCLE PERMETTANT DE SUPPRIMER LE PRODUIT CHOISIS DU LOCALDSTORAGE ET DE L'HTML
+        const deleteItem = document.getElementsByClassName("deleteItem");
+
+        Array.from(deleteItem).map(function (d) {
+          d.addEventListener("click", function () {
+
+            let articleParent = this.closest("article");
+            articleParent.remove("article");
+
+            let dataColor = articleParent.dataset.color;
+            let dataId = articleParent.dataset.id;
+            let productIndex = getProducts.findIndex((element) => element.productID == dataId && element.color == dataColor);
+            getProducts.splice(productIndex, 1)
+            localStorage.setItem(`product`, JSON.stringify(getProducts));
+            affichagePanier();
+
+          })
+        })
+
       })
-    })
-})
+  })
 }
 affichagePanier()
 
+// Formulaire Validation
 const submit = document.getElementById("order")
-
-submit.addEventListener("click", function () {
+submit.addEventListener("click", function (e) {
+  e.preventDefault()
 
   let contact = {
     firstName: document.getElementById("firstName").value,
     lastName: document.getElementById("lastName").value,
     email: document.getElementById("email").value,
+    address: document.getElementById("address").value,
+    city: document.getElementById("city").value,
   }
-  // PRENOM / NOM / VILLE
-  const name = /^[a-z ,.'-]+$/i;
 
-  // EMAIL
+  // REGEX
+  const name = /^[a-z ,.'-]+$/i;
   const email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-  if (
-    (email.test(contact.email)) &
-    (name.test(contact.firstName)) &
-    (name.test(contact.lastName))
-  ) {
+  let error = false;
 
-    alert("Formulaire envoyé")
-  }
-  else {
-
-    alert("Erreur");
+  if (!name.test(contact.firstName)) {
+    document.getElementById("firstNameErrorMsg").textContent = "Prénom invalide"
+    error = true;
   }
 
+  if (!name.test(contact.lastName)) {
+    document.getElementById("lastNameErrorMsg").textContent = "Nom invalide"
+    error = true;
+  }
+
+  if (!name.test(contact.address)) {
+    document.getElementById("addressErrorMsg").textContent = "Adresse invalide"
+    error = true;
+  }
+
+  if (!name.test(contact.city)) {
+    document.getElementById("cityErrorMsg").textContent = "Ville invalide"
+    error = true;
+  }
+
+  if (!email.test(contact.email)) {
+    document.getElementById("emailErrorMsg").textContent = "Adresse e-mail invalide"
+    error = true;
+  }
+
+  if (!error) {
+
+    let contactSubmit = {
+      firstName: contact.firstName,
+      lastName: contact.lastName,
+      address: contact.address,
+      city: contact.city,
+      email: contact.email,
+    }
+
+    let productIds = []
+
+    let getProducts = JSON.parse(localStorage.getItem(`product`));
+    // console.log(getProducts.productID);
+
+    getProducts.map(function (p) {
+      productIds.push(p.productID)
+    })
+
+    // console.log(contactSubmit);
+    // console.log(productIds);
+
+    let infoSubmit = [contactSubmit, productIds]
+    console.log(infoSubmit);
+
+
+  }
 })
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,17 +1,17 @@
 const url = "http://localhost:3000/api/products";
 const card = document.getElementById("items");
 
-  fetch(url)
-      .then(response => response.json())
-      .then(products => {
+fetch(url)
+  .then(response => response.json())
+  .then(products => {
 
-          products.map(function(product) {
-            card.innerHTML += `<a href="./product.html?id=${product._id}">
+    products.map(function (product) {
+      card.innerHTML += `<a href="./product.html?id=${product._id}">
               <article>
                 <img src="${product.imageUrl}" alt="${product.altTxt}">
                 <h3 class="productName"> ${product.name}</h3>
                 <p class="productDescription">${product.description}</p>
               </article>
             </a>`
-          })
-})
+    })
+  })
